7cdd512e10dd5e91e15f33b6de41fe9496420f34
VasilievZhenya <vasilievzhenya98@gmail.com>
2020-02-10 11:33:49 +0200

    Trying fix heroku error

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,10 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import telegram.bot.GifMakerBot;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
@@ -16,6 +20,14 @@ public class Main {
             api.registerBot(new GifMakerBot());
         } catch (TelegramApiRequestException e) {
             log.error("Telegram api exception.", e);
+        }
+
+        try (ServerSocket serverSocket = new ServerSocket(Integer.valueOf(System.getenv("PORT")))) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+            }
+        } catch (IOException e) {
+            log.error("Error while listening port.", e);
         }
     }
 }
